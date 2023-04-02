dc504ccbd151ac2f3cdfed9a971d6ff9b785c742
biubu <w@biubu.cn>
2023-04-02 18:45:54 +0800

    更新配置, 删除多余的导入

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,32 @@
 <template>
-  <div class="mapdiv" ref="mapdiv"></div>
+    <div class="mapdiv" ref="mapdiv"></div>
 </template>
 
 <script lang="ts" setup>
-import WebMap from "@arcgis/core/WebMap";
 import MapView from "@arcgis/core/views/MapView";
-import { onMounted, ref } from "vue";
+import {onMounted, ref} from "vue";
 import Map from '@arcgis/core/Map';
-// import MapView from '@arcgis/core/views/MapView';
+
 const mapdiv = ref();
 
 onMounted(() => {
-const map = new Map({
-  basemap: "topo-vector",
-});
+    const map = new Map({
+        basemap: "topo-vector",
+    });
 
-const view = new MapView({
-  container: mapdiv.value,
-  map: map,
-  center:[108,34],
-  zoom:5,
-});
+    const view = new MapView({
+        container: mapdiv.value,
+        map: map,
+        center: [108, 34],
+        zoom: 5,
+    });
 })
 </script>
 
 <style scoped>
 .mapdiv {
-  padding: 0;
-  margin: 0;
-  height: 100vh;
+    padding: 0;
+    margin: 0;
+    height: 100vh;
 }
 </style>
